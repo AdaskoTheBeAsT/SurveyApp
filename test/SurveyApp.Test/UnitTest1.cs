addcb171d1b5a2a8eb6e1f3bf9c533b8ee3bde9b
Adam Pluciński <adaskothebeast@gmail.com>
2019-01-26 01:42:10 +0100

    Merge branch 'master' of https://github.com/AdaskoTheBeAsT/SurveyApp
    # Conflicts:
    #	azure-pipelines.yml

---
+++ test/SurveyApp.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SurveyApp.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // no op - will be done in a moment ;)
+        }
+    }
+}
